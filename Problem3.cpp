b53a587613f44a291dbc56565304d84b1e1fe502
kzyma <zymakenneth@gmail.com>
2014-01-04 15:31:40 -0500

    setting up repo

---
+++ Problem3.cpp
@@ -0,0 +1,12 @@
+/****************************************************
+*   File:   Problem3.cpp
+*   Author: Kenneth Zyma
+*   Date:   January, 2014
+*   This file is the solution to Problem 3 found on
+*       project Euler. http://projecteuler.net/
+*****************************************************/
+
+int main(){
+    std::cout<<"Hello World"<<std::endl;
+    return 0;
+}
