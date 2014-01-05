47c7da4e01008a8bdc00bbf82539df16e6a28536
kzyma <zymakenneth@gmail.com>
2014-01-04 21:24:15 -0500

    Problem 3 completed

--- Problem3.cpp
+++ Problem3.cpp
@@ -1,12 +1,34 @@
-/****************************************************
+/********************************************************
 *   File:   Problem3.cpp
 *   Author: Kenneth Zyma
 *   Date:   January, 2014
 *   This file is the solution to Problem 3 found on
 *       project Euler. http://projecteuler.net/
-*****************************************************/
+*
+*   Problem: What is the largest prime factor of 
+*               the number 600851475143?
+*********************************************************/
+#include<iostream>
+#include <math.h> 
+
+long findLargestPrimeFactor(long);
+bool isPrime(long);
 
 int main(){
-    std::cout<<"Hello World"<<std::endl;
+    long largestFactor;
+    largestFactor = findLargestPrimeFactor(600851475143);
+    std::cout<<"Largest Prime Factor is: "<<largestFactor<<std::endl;
     return 0;
+}
+
+long findLargestPrimeFactor(long number){
+    long count;
+    for (long count=2; count<(number/2); count++) {
+        if((number%count)==0){
+            number /= count;
+            //reset counter
+            count=2;
+        }
+    }
+    return number;
 }
