d63db8f15714d3be6e3fd3060c5cdfcb8c48f31d
kzyma <zymakenneth@gmail.com>
2014-01-12 14:03:01 -0500

    Problem 10 complete

---
+++ Problem10.cpp
@@ -0,0 +1,41 @@
+/*************************************************************
+ *   File:   Problem10.cpp
+ *   Author: Ken Zyma
+ *   Date:   January, 2014
+ *   This file is the solution to Problem 10 found on
+ *       project Euler. http://projecteuler.net/
+ *
+ *   Problem: Find the sum of all the primes below two million.
+ *
+ *************************************************************/
+
+#include<iostream>
+#include <math.h>
+
+#include "DataStructures/dynamicArray/dynamic_array.h"
+
+bool isPrime(long);
+
+int main(){
+    //init with sum of all even primes
+    long sum=17;
+    long max=2000000;
+    
+    for (long i= 8; i<max; i++) {
+        if (isPrime(i)) {
+            sum+=i;
+        }
+    }
+    
+    std::cout<<"Sum: "<<sum<<std::endl;
+    return 0;
+}
+
+bool isPrime(long number){
+    for (long i=2; i<=sqrt(number); i++) {
+        if (number%i==0) {
+            return false;
+        }
+    }
+    return true;
+}
